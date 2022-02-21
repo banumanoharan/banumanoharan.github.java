e202a9461bd50cec55f29203cf5b5b243f60dee9
banumanoharan <banumanoharan83@gmail.com>
2022-02-21 18:55:20 +0530

    Test class for combining two list in a single list

--- src/collection/TestClass.java
+++ src/collection/TestClass.java
@@ -13,8 +13,13 @@ public class TestClass {
         list2.add("List2Element1");
         list2.add("List2Element2");
         list2.add("List2Element3");
-        System.out.println(list1);
-        System.out.println(list2);
-
+        System.out.println("List 1:"+list1);
+        System.out.println("List 2:" +list2);
+        List list3 = new ArrayList();
+        list3.addAll(list1);
+        list3.addAll(list2);
+        list3.add("List3Element1");
+        list3.add("List3Element2");
+        System.out.println("List 3:"+list3);
     }
 }
